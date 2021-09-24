47503212875276ffe4314cb5f17a9b460a677e8d
Charlie Ford <charles.ford1978@gmail.com>
2021-09-24 09:57:33 -0400

    Add project files.

---
+++ BootcampDailyCost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BootcampDailyCostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var BootcampCost = 15500m;
+            var BootcampDays = 65;
+
+            Console.WriteLine($"Each day of Bootcamp costs ${BootcampCost / BootcampDays}.");
+            Console.WriteLine("So try not to miss any days.");
+        }
+    }
+}
