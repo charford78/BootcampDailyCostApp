3de721c5c999803630810929d2fad2236b1b527b
Charlie Ford <charles.ford1978@gmail.com>
2021-12-13 22:40:15 -0500

    Added more string variables to represent parts of the message.  Makes the Console.writeline line much easier.

--- BootcampDailyCost.cs
+++ BootcampDailyCost.cs
@@ -8,9 +8,13 @@ namespace BootcampDailyCostApp
         {
             var BootcampCost = 15500m;
             var BootcampDays = 65;
+            var PreMessage = "Each day of Bootcamp costs $";
+            var PostMessage = ".";
+            var Message = $"{PreMessage}{BootcampCost / BootcampDays}{PostMessage}";
 
-            Console.WriteLine($"Each day of Bootcamp costs ${BootcampCost / BootcampDays}.");
+            Console.WriteLine($"{Message}");
             Console.WriteLine("So try not to miss any days.");
+            System.Diagnostics.Debug.WriteLine($"{Message}");
         }
     }
 }
